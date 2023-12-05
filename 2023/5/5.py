e7d51a76adfaa01e62f657ff0db574371bc3e49f
Carl <c.a.wingardh@gmail.com>
2023-12-05 11:12:32 +0100

    day 5 part 2 (broken, but just managed to eek out a win)

--- 2023/5/5.py
+++ 2023/5/5.py
@@ -31,9 +31,14 @@ def get_map(from_keyword: str, to_keyword: str):
   mapping = list(map(lambda row: list(map(lambda number: int(number), row.split())), rows))
   return mapping
 
+def get_seed_pairs(iterable):
+    seed_iterator = iter(iterable)
+    return list(zip(seed_iterator, seed_iterator))
 
 seeds = re.search("^seeds: (.*)\n", input).groups()[0].split()
 seeds = list(map(lambda seed: int(seed), seeds))
+
+seed_pairs = get_seed_pairs(seeds)
 
 maps = {
   "seed_to_soil": get_map("seed", "soil"),
@@ -45,37 +50,126 @@ maps = {
   "humidity_to_location": get_map("humidity", "location"),
 }
 
+map_legend = 'sfwlthl'
+
 print("Entries in")
 for key, map in maps.items():
     print(f"{key}: {len(map)}")
 
 print("Seeds: ", end="")
-for seed in seeds:
-    print(seed, end=" ")
+for seed_pair in seed_pairs:
+    print(seed_pair, end=" ")
 print()
 
 print("---")
 
+ 
+def create_new_lookup(lookup_start, entry_difference, entry_length):
+  converted_lookup_start = lookup_start - entry_difference
+  new_lookup = (converted_lookup_start, entry_length, -entry_difference)
+  return new_lookup
+    
+
+def get_new_lookup_numbers(lookup_start, lookup_length, entry_length):
+  lookup_start = lookup_start + entry_length
+  lookup_length = lookup_length - entry_length
+  return (lookup_start, lookup_length)
+
+def calc_lookups(start: int, length: int, map: str | list[int]):
+    lookups: list[tuple] = []
+    
+    lookup_start = start
+    lookup_length = length
+    non_converted_start = None
+    non_converted_amount = 0
+    while lookup_length > 0:
+      found_lookup = False
+
+      for entry in map:
+          entry_start = entry[1]
+          entry_end = entry[1] + entry[2]
+          entry_length = entry[2]
+          entry_difference = entry[1] - entry[0]
+          lookup_start_in_entry = lookup_start >= entry_start and lookup_start < entry_end
+          min_length = min(entry_start + entry_length - lookup_start, lookup_length)
+
+          if lookup_start_in_entry:
+              if non_converted_start: # put in non-conversion lookup
+                lookups.append(create_new_lookup(non_converted_start, 0, non_converted_amount))
+                non_converted_start = None
+                non_converted_amount = 0
+                 
+              lookups.append(create_new_lookup(lookup_start, entry_difference, min_length))
+              (lookup_start, lookup_length) = get_new_lookup_numbers(lookup_start, lookup_length, min_length)
+              found_lookup = True
+              break
+      
+      if not found_lookup:
+        if not non_converted_start:
+           non_converted_start = lookup_start
+           non_converted_amount = 1
+        else:
+          non_converted_amount = non_converted_amount + 1
+        (lookup_start, lookup_length) = get_new_lookup_numbers(lookup_start, lookup_length, 1)
+      
+    if non_converted_start: # put in non-conversion lookup
+      lookups.append(create_new_lookup(non_converted_start, 0, non_converted_amount))
+      non_converted_start = None
+      non_converted_amount = 0
+    return lookups
+   
+
+def convert(start: int, length: int, maps: list[str | list[int]]):
+    if len(maps) < 1:
+       return start
+    map = maps[0]
+    
+
+
+    lookups = calc_lookups(start, length, map)
+
+    minimum = math.inf
+    for lookup in lookups:
+      print(f"{map_legend[::-1][len(maps) - 1:][::-1]}: ", end='')
+      print(format_lookup(lookup))
+
+      child_minimum = convert(lookup[0], lookup[1], maps[1:])
+
+      if child_minimum < minimum:
+        print(f"{map_legend[::-1][len(maps) - 1:][::-1]}: ", end='')
+        print(child_minimum)
+        minimum = child_minimum
+
+    return minimum
+        
+def format_lookup(lookup):
+    return f"{lookup[0]}...{lookup[0] + lookup[1] - 1} ({lookup[2]}) (amt: {lookup[1]})"
+   
+
 answer = math.inf
 
-for seed in seeds:
-    print(f"Processing {seed}")
+seed_answers = {}
+map_answers = {
+  "seed_to_soil": {},
+  "soil_to_fertilizer": {},
+  "fertilizer_to_water": {},
+  "water_to_light": {},
+  "light_to_temperature": {},
+  "temperature_to_humidity": {},
+  "humidity_to_location": {},
+}
 
-    conversion = seed
 
-    for map_name, map in maps.items():
-        new_conversion = conversion
-        for entry in map:
-          seed_in_entry = conversion >= entry[1] and conversion < entry[1] + entry[2]
-          if seed_in_entry:
-            new_conversion = conversion - (entry[1] - entry[0])
-            print(f"{conversion} -{map_name}:{entry}-> {new_conversion}")
-        if conversion == new_conversion:
-            print(f"No change in {map_name}")
-        conversion = new_conversion
+for seed_pair in seed_pairs:
+    print(f"[{seed_pair}]")
 
-    if conversion < answer:
-        answer = conversion
+    lookup_start = seed_pair[0]
+    lookup_end = seed_pair[0] + seed_pair[1]
+    lookup_length = seed_pair[1]
+    
+    result = convert(lookup_start, lookup_length, list(maps.values()))
+    if result < answer:
+      answer = result
 
 
 #####
