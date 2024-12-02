ebbafb9a5a0f528047736c1ae8e08ad4738259fd
Carl Wingardh <c.a.wingardh@gmail.com>
2024-12-02 09:38:13 +0100

    2024 day 1

---
+++ 2024/1/1.py
@@ -0,0 +1,120 @@
+# Create files called input.txt, test_input.txt and test_answer.txt
+# If nodemon is installed, run using nodemon.cmd --exec py <path to file> -t
+
+import os
+
+import aocd
+import dotenv
+import inquirer
+
+dotenv.load_dotenv()
+
+dir = os.path.dirname(__file__)
+
+def try_get_multiple_file_contents(*filenames: list[str]):
+  result = []
+  for filename in filenames:
+    path = os.path.join(dir, filename)
+    if os.path.isfile(path):
+      file = open(file=path, mode="r")
+      content = file.read()
+      file.close()
+    else:
+      content = ""
+    result.append(content)
+  return result
+     
+   
+[input, test_input, test_answer_a, test_answer_b] = try_get_multiple_file_contents("input.txt", "test_input.txt", "test_answer_a.txt", "test_answer_b.txt")
+
+#####
+
+def get_result(input: str, part_b: bool = False):
+    
+    lines = input.split("\n")
+    group_a_list = []
+    group_b_list = []
+    for line in lines:
+      parts = line.split("   ")
+      group_a_list.append(int(parts[0]))
+      group_b_list.append(int(parts[1]))
+
+    if not part_b:
+
+      group_a_list.sort()
+      group_b_list.sort()
+
+      total = 0
+
+      for i in range(len(lines)):
+        total += abs(group_a_list[i] - group_b_list[i])
+
+      return str(total)
+
+    if part_b:
+      memo = {}
+
+      product_sum = 0
+
+      for location_a in group_a_list:
+        mulitplicator = 0
+        if location_a in memo:
+          product_sum += memo[location_a]
+        else:
+          for location_b in group_b_list:
+            if location_a == location_b:
+              mulitplicator += 1
+          
+          product_sum += location_a * mulitplicator
+          memo[location_a] = product_sum
+      
+      return product_sum
+
+
+
+#####
+
+def check_result(result: str, answer: str):
+  if answer == "":
+    print("⏹ ")
+  else:
+    if str(result) == answer:
+        print(f"🟩 {result}")
+        return True
+    if str(result) != answer:
+        print(f"🟥 {result} (expected {answer})")
+  return False
+
+print("-- TEST A --")
+test_result_a = get_result(test_input)
+test_a_success = check_result(test_result_a, test_answer_a)
+print("---")
+print("-- TEST B --")
+test_result_b = get_result(test_input, True)
+test_b_success = check_result(test_result_b, test_answer_b)
+print("---")
+
+if test_b_success:
+  if inquirer.confirm("Test succeeded on one part b, run on real data?"):
+    print("RUNNING PART B")
+    result = get_result(input, True)
+
+    print("RESULT PART B: ", end="")
+    print(result)
+
+    if inquirer.confirm(f"Submit result ({result})?"):
+       [year, day] = os.path.dirname(__file__).split(os.path.sep)[-2:]
+       aocd.submit(result, part="b", day=int(day), year=int(year))
+
+if test_a_success:
+  if inquirer.confirm("Test succeeded on one part a, run on real data?"):
+    print("RUNNING PART A")
+    result = get_result(input)
+
+    print("RESULT PART A: ", end="")
+    print(result)
+
+    if inquirer.confirm(f"Submit result ({result})?"):
+       [year, day] = os.path.dirname(__file__).split(os.path.sep)[-2:]
+       aocd.submit(result, part="a", day=int(day), year=int(year))
+
