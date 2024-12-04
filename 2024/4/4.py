618e3ce7e281a23acbc44bfa1be0484c4859f1ef
Carl Wingardh <c.a.wingardh@gmail.com>
2024-12-04 09:26:50 +0100

    setup 2024 day 4

---
+++ 2024/4/4.py
@@ -0,0 +1,9 @@
+from libraries.solution_manager import PuzzleSolution
+
+class Solution(PuzzleSolution):
+    
+    def get_answer_a(self, input: str) -> int | float | str:
+        return ""
+
+    def get_answer_b(self, input: str) -> int | float | str:
+        return ""
