924039e366987b6368826123aaf59694b40088a2
Carl Wingardh <c.a.wingardh@gmail.com>
2024-12-05 16:47:53 +0100

    added time profiling to runner

--- helpers/runner.py
+++ helpers/runner.py
@@ -1,9 +1,12 @@
+import datetime
 import multiprocessing
 import os
+import time
 import traceback
 from typing import Tuple
 
 import aocd
+import humanize
 import inquirer
 from libraries import file_manager
 from libraries.solution_manager import SolutionManager
@@ -32,85 +35,123 @@ class Runner:
     def on_change(self):
         self.solution_manager.load_solution()
 
-        print("============")
-        print("🧪 TEST A   ")
-        print("------------")
+        print("======================================")
+        print("    🧪 TEST A 🧪")
 
         if self.check_solved("a"):
-            print("Already solved!")
+            print("✅  Already solved!")
         else:
 
             test_input_a = self.try_get_file_contents(TEST_INPUT_FILE_NAME)
-            test_answer_a = self.try_get_file_contents(TEST_ANSWER_A_FILE_NAME)
+            test_answer_b = self.try_get_file_contents(TEST_ANSWER_A_FILE_NAME)
 
             try:
+                print()
+                print("--- 🗒️  Logs --------------------------")
+                start_time = time.time()
                 test_solution_a = self.solution_manager.run_solution_a(test_input_a)
-                print("------------")
-                test_a_success = Runner.check_result(test_solution_a, test_answer_a)
+                end_time = time.time()
+                print()
+                print("--- 🗳️  Result ------------------------")
+                running_time = humanize.precisedelta(datetime.timedelta(seconds=end_time - start_time), minimum_unit="microseconds")
+                print(f"⏱️  Finished in {running_time}")
+                test_a_success = Runner.check_result(test_solution_a, test_answer_b)
+                print()
+                print("--------------------------------------")
 
                 if test_a_success:
+                    print()
                     if inquirer.confirm("Test succeeded on one part a, run on real data?"):
-                        print("------------")
+                        print("--------------------------------------")
                         input_a = self.try_get_file_contents(INPUT_FILE_NAME)
+                        
+                        start_time = time.time()
                         result = self.solution_manager.run_solution_a(input_a)
+                        end_time = time.time()
+                        
+                        print()
+                        print("--- 🗳️  Result ------------------------")
 
-                        print("RESULT PART A: ", end="")
+                        running_time = humanize.precisedelta(datetime.timedelta(seconds=end_time - start_time), minimum_unit="microseconds")
+                        print(f"⏱️  Finished in {running_time}")
+
+                        print("🟦 ", end="")
                         print(result)
 
+                        print()
                         if inquirer.confirm(f"Submit result ({result})?"):
                             aocd.submit(result, part="a", year=watch_date[0], day=watch_date[2])
                             self.puzzle = aocd.models.Puzzle(year=watch_date[0], day=watch_date[2])
                             if self.check_solved("a"):
-                                print("Success!")
+                                print("🎉  Success!")
 
             except Exception as e:
                 traceback.print_exc()
+                print("--------------------------------------")
+                print()
                 print(f"⚠️  Running failed because of exception: {e}")
+                print()
         
-        print("============")
+        print("======================================")
         print("")
-        print("============")
-        print("🧪 TEST B   ")
-        print("------------")
+        print("======================================")
+        print("  🧪🧪 TEST B 🧪🧪")
 
         if self.check_solved("b"):
-            print("Already solved!")
+            print("✅  Already solved!")
         else:
             test_input_b = self.try_get_file_contents(TEST_INPUT_B_FILE_NAME)
             if test_input_b is None:
                 test_input_b = self.try_get_file_contents(TEST_INPUT_FILE_NAME)
-            test_answer_a = self.try_get_file_contents(TEST_ANSWER_B_FILE_NAME)
+            test_answer_b = self.try_get_file_contents(TEST_ANSWER_B_FILE_NAME)
 
             try:
+                print()
+                print("--- 🗒️  Logs --------------------------")
+                start_time = time.time()
                 test_solution_b = self.solution_manager.run_solution_b(test_input_b)
-                print("------------")
-                print("Result: ")
-                test_b_success = Runner.check_result(test_solution_b, test_answer_a)
+                end_time = time.time()
+                running_time = humanize.precisedelta(datetime.timedelta(seconds=end_time - start_time), minimum_unit="microseconds")
+                print(f"Finished in {running_time}")
+                test_b_success = Runner.check_result(test_solution_b, test_answer_b)
+                print()
+                print("--------------------------------------")
 
                 if test_b_success:
                     if inquirer.confirm("Test succeeded on one part b, run on real data?"):
-                        print("------------")
+                        print("--------------------------------------")
                         input_b = self.try_get_file_contents(INPUT_B_FILE_NAME)
                         if input_b is None:
                             input_b = self.try_get_file_contents(INPUT_FILE_NAME)
+                        
+                        start_time = time.time()
                         result = self.solution_manager.run_solution_b(input_b)
+                        end_time = time.time()
+                        
+                        print()
+                        print("--- 🗳️  Result ------------------------")
 
-                        print("RESULT PART B: ", end="")
+                        running_time = humanize.precisedelta(datetime.timedelta(seconds=end_time - start_time), minimum_unit="microseconds")
+                        print(f"⏱️  Finished in {running_time}")
+
+                        print("🟦 ", end="")
                         print(result)
 
+                        print()
                         if inquirer.confirm(f"Submit result ({result})?"):
                             aocd.submit(result, part="b", year=watch_date[0], day=watch_date[2])
                             self.puzzle = aocd.models.Puzzle(year=watch_date[0], day=watch_date[2])
                             if self.check_solved("b"):
-                                print("Success!")
-
+                                print("🎉  Success!")
 
             except Exception as e:
                 traceback.print_exc()
                 print(f"⚠️  Running failed because of exception: {e}")
+                print()
+                print("--------------------------------------")
+        
+        print("======================================")
             
-        print("============")
-
     def check_solved(self, part: str):
         try:
             if part == "a":
