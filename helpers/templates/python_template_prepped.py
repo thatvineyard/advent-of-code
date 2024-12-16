2b2b045f59fdc202840b76d6ae8b8d16382ade83
Carl Wingardh <c.a.wingardh@gmail.com>
2024-12-16 16:31:09 +0100

    template improvements

--- helpers/templates/python_template_prepped.py
+++ helpers/templates/python_template_prepped.py
@@ -1,3 +1,5 @@
+from ast import Set
+from enum import Enum
 from typing import List, Tuple
 from libraries.solution_manager import PuzzleSolution
 
@@ -6,63 +8,120 @@ class Element:
         self.character = character
         
     def __repr__(self):
-        return f"[{self.character}]"
+        return f"({self.character})"
 
-class Coordinate:
+class Vector:
     def __init__(self, x: int, y: int):
         self.x = x
         self.y = y
 
     def __add__(self, other):
-        if not isinstance(other, Coordinate):
-            raise NotImplemented(
-                f"Tried adding coordinate ({self}) with something that's not a coodinate ({other})"
+        if not isinstance(other, Vector):
+            raise NotImplementedError(
+                f"Tried adding {Vector.__name__} ({self}) with something that's not a {Vector.__name__} ({other})"
             )
 
-        return Coordinate(self.x + other.x, self.y + other.y)
+        return Vector(self.x + other.x, self.y + other.y)
 
     def __sub__(self, other):
-        if not isinstance(other, Coordinate):
-            raise NotImplemented(
-                f"Tried to perform subtraction on coordinate ({self}) with something that's not a coodinate ({other})"
+        if not isinstance(other, Vector):
+            raise NotImplementedError(
+                f"Tried to perform subtraction on {Vector.__name__} ({self}) with something that's not a {Vector.__name__} ({other})"
             )
 
-        return Coordinate(self.x - other.x, self.y - other.y)
+        return Vector(self.x - other.x, self.y - other.y)
 
     def __repr__(self):
-        return f"[{self.x}, {self.y}]"
+        return f"({self.x}, {self.y})"
 
     def __eq__(self, value):
-        if not isinstance(value, Coordinate):
-            raise NotImplemented(
-                f"Tried to perform equals check on coordinate ({self}) with something that's not a coodinate ({other})"
+        if not isinstance(value, Vector):
+            raise NotImplementedError(
+                f"Tried to perform equals check on {Vector.__name__} ({self}) with something that's not a {Vector.__name__} ({value})"
             )
         return self.x == value.x and self.y == value.y
 
     def __hash__(self):
         return self.__repr__().__hash__()
 
+class Direction(Enum):
+    UP = ("^", Vector(0, -1))
+    RIGHT = (">", Vector(1, 0)) 
+    DOWN = ("v", Vector(0, 1)) 
+    LEFT = ("<", Vector(-1, 0)) 
+
+    def __repr__(self):
+        return self.value[0]
+    
+class GridElement(Element):
+    def __init__(self, character, location: Vector):
+        super().__init__(character)
+        self.location = location
+        
+    def __repr__(self):
+        return f"({super().__repr__}@{self.location})"
+
 class Grid:
-    def __init__(self, width, height):
+    def __init__(self, width: int, height: int, elements: List[GridElement] = []):
         self.width = width
         self.height = height
-    
-    def is_in_bounds(self, coordinate: Coordinate) -> bool:
+        self.element_map: Set[Vector, GridElement] = {}
+        for element in elements:
+            self.add_element(element)
+
+    def add_element(self, element: GridElement):
+        if element.location in self.element_map:
+            raise KeyError(f"An element ({self.element_map[element.location]}) is already placed at {element.location}")
+
+        self.element_map[element.location] = element
+
+    def move_element(self, element: GridElement, new_location: Vector) -> GridElement:
+        if new_location in self.element_map:
+            raise KeyError(f"Tried to move element ({element}) to location ({new_location}) but another element ({self.element_map[element.location]}) is already there")
+        if self.element_map[element.location] != element:
+            raise RuntimeError(f"Elements ({element}) location did not match grid's element map")
+
+        old_location = element.location
+        self.element_map[old_location] = None
+        element.location = new_location
+        self.add_element(element)
+        return element
+
+
+    def is_in_bounds(self, coordinate: Vector) -> bool:
         return coordinate.x < self.width and coordinate.x >= 0 and coordinate.y < self.height and coordinate.y >= 0
     
-    def each_coordinate(width, height):
-        for y in range(height):
-            for x in range(width):
-                yield Coordinate(x, y)
+    def each_coordinate(self):
+        for y in range(self.height):
+            for x in range(self.width):
+                yield Vector(x, y)
+    
+    def each_element(self):
+        for element in self.element_map.values():
+            yield element
+    
+    def __repr__(self):
+        result = ""
+        current_y_coord = 0
+        for coordinate in self.each_coordinate():
+            if current_y_coord != coordinate.y:
+                result += "\n"
+                current_y_coord = coordinate.y
+            if coordinate in self.element_map:
+                result += self.element_map[coordinate].short_name()
+            else:
+                result += "."
+        return result
 
 class Solution(PuzzleSolution):
     
     def get_answer_a(self, input: str) -> int | float | str:
-        elements = self.get_row_elements(input)
-        # grid, elements = self.get_grid_elements(input)
+        # elements = self.get_row_elements(input)
+        # for element in elements:
+        #     print(element)
         
-        for element in elements:
-            print(element)
+        # grid, elements = self.get_grid_elements(input)
+        # print(grid)
         
         return ""
 
